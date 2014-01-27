ad14b10bea71907ec1422a889cc8d36819e9a6ef
dan125 <dberezhinskiy@gmail.com>
2014-01-27 15:22:40 -0800

    basic complete

---
+++ danb/danb/basic.js
@@ -0,0 +1,22 @@
+// basic.js
+
+// Part 1
+var myArray = [1, 2, 3]
+
+// Part 2
+myArray.push(4);
+
+// Part 3
+myArray.shift(1);
+
+// Part 4
+console.log(myArray);
+
+// Part 5
+myArray.unshift(0.5);
+
+// Part 6
+myArray.pop(4);
+
+// Part 7
+console.log(myArray);
